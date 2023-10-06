02e3ccf3291b44bd22346034e07771e318b8acf6
Tiago Fonseca <t.fonseca@ua.pt>
2023-10-07 00:21:11 +0100

    Finished Ex2

--- P3/02-2-shm+sem/code/question2/bounded-buffer/ex2.cpp
+++ P3/02-2-shm+sem/code/question2/bounded-buffer/ex2.cpp
@@ -15,31 +15,34 @@ int main(void)
 
     // create the semaphores for the odd numbers and the even number
     int sems = psemget(IPC_PRIVATE, 2, IPC_CREAT | 0600);
-
     // ask the user for a number
     int n;
 
     printf("Enter a number: ");
     scanf("%d", &n);
 
+
+    psem_up(sems, !n%2); //up the semaphore of the first number to be printed
+
+
     pid_t pid = pfork();
     
-    while( *pcounter <= n)
-    {   
-        if (pid == 0) //print odds
-        {   
-            psem_up(sems, EVEN);
+    while( *pcounter < n) //se for <=n, um dos processos fica bloqueado
+    {
+        if (pid == 0) //print odds child
+        {
             psem_down(sems, ODD);
             printf("(%d) %d\n", pid, (*pcounter)++);
+            psem_up(sems, EVEN);
         }
-        else
+        else //print even parent
         {
             psem_down(sems, EVEN);
+            printf("(%d) %d\n", pid, (*pcounter)++);    
             psem_up(sems, ODD);
-            printf("(%d) %d\n", pid, (*pcounter)++);
+            
         }
+        //printf("COUNTER %d - PID %d\n", *pcounter, pid);
     }
-
-
     return EXIT_SUCCESS;
 }
